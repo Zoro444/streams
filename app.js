a9aae9971936d53266265505f9ed4dfedd760274
unknown <zoro.simonyan1995@gmail.com>
2023-06-04 22:04:29 -0800

    add description

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 import readAndWriteFile from "./stream-functions/read-write-file.js";
 import fs from "fs";
 import path from "path";
+import os from "os";
 
 process.stdin.setEncoding('utf-8');
+
+console.log(`Welcome ${os.userInfo().username}!\n
+This is a command-line application that reads data from an input file,
+performs a specified operation on the data using streams, 
+and writes the transformed data to an output file.
+Select input file path, output file path and operation!`);
 
 process.stdin.on('data', (data) => {
   data = data.trim();
